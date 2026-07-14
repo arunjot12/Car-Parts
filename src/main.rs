34fbd799a4f77f1dfc00c67f462a8ed50ba7960f
Arunjot Singh ATR592 <arunjot@antiersolutions.com>
2026-07-14 13:15:50 +0530

    chore: Added dependency for hashing

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,16 @@ use axum::{
     Router
 };
 
+pub mod data;
+
 #[tokio::main]
 async fn main() {
+
+   let email = "arunjotsingh@gmail.com";
+   let password = "arunjot";
+
+   let hashed_password = password
+
    let app = Router::new().route("/",get(root)).route("/hello",get(run));
    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
    axum::serve(listener,app).await.unwrap();
